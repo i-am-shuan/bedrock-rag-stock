bb6207363df6a8d9af04108bbb77aa19743cb611
hochya <yh9589@gmail.com>
2024-04-25 01:38:03 +0900

    fix for claude3

--- 4_stock_analysis/stock_analysis_lib.py
+++ 4_stock_analysis/stock_analysis_lib.py
@@ -30,7 +30,7 @@ def get_llm():
     model_parameter = {"temperature": 0.0, "top_p": .5, "max_tokens_to_sample": 2000}
     llm = Bedrock(
         credentials_profile_name=os.environ.get("BWB_PROFILE_NAME"), #sets the profile name to use for AWS credentials (if not the default)
-        region_name=os.environ.get("BWB_REGION_NAME"), #sets the region name (if not the default)
+        region_name="us-east-1", #sets the region name (if not the default)
         endpoint_url=os.environ.get("BWB_ENDPOINT_URL"), #sets the endpoint URL (if necessary)
         model_id="anthropic.claude-v2", #set the foundation model
         model_kwargs=model_parameter,
@@ -51,7 +51,7 @@ def get_claude3():
         "temperature": 0.0,
         "top_k": 250,
         "top_p": 1,
-        "stop_sequences": ["\n\nSQLQuery: ", "\n\nHuman: "],
+        "stop_sequences": ["\n\nHuman: "],
     }
 
     model = BedrockChat(
@@ -77,7 +77,7 @@ def get_db_chain(prompt):
 def get_stock_ticker(query):
     template = """You are a helpful assistant who extract company name from the human input.Please only output the company"""
     human_template = "{text}"
-    llm = get_claude3()
+    llm = get_llm()
 
     chat_prompt = ChatPromptTemplate.from_messages([
         ("system", template),
@@ -146,7 +146,8 @@ import requests
 def google_query(search_term):
     if "news" not in search_term:
         search_term=search_term+" stock news"
-    url=f"https://www.google.com/search?q={search_term}"
+    
+    url=f"https://www.google.com/search?q={search_term}&tbm=nws"
     url=re.sub(r"\s","+",url)
     return url
 
@@ -158,21 +159,23 @@ def get_recent_stock_news(company_name):
     res=requests.get(g_query,headers=headers).text
     soup=BeautifulSoup(res,"html.parser")
     news=[]
+    for n in soup.find_all("div","n0jPhd ynAwRc MBeuO nDgy9d"):
+        news.append(n.text)
     for n in soup.find_all("div","n0jPhd ynAwRc tNxQIb nDgy9d"):
         news.append(n.text)
     for n in soup.find_all("div","IJl0Z"):
         news.append(n.text)
 
-    if len(news)>6:
-        news=news[:4]
+    if len(news)>11:
+        news=news[:10]
     else:
         news=news
     news_string=""
     for i,n in enumerate(news):
         news_string+=f"{i}. {n}\n"
-    top5_news="Recent News:\n\n"+news_string
+    top10_news="Recent News:\n\n"+news_string
     
-    return top5_news
+    return top10_news
 
 def stock_news_search(company_name):
     search=DuckDuckGoSearchRun()
@@ -185,7 +188,7 @@ def get_financial_statements(ticker):
         ticker=ticker.split(".")[0]
     else:
         ticker=ticker
-    company = yf.Ticker(ticker)
+    company = yf.Ticker(ticker.strip())
     balance_sheet = company.balance_sheet
     if balance_sheet.shape[1]>=3:
         balance_sheet=balance_sheet.iloc[:,:3]    # Only captures last 3 years of data
@@ -223,49 +226,32 @@ tools=[
 
 ]
 
-
-from langchain.agents import initialize_agent 
-def initializeAgent():
-    agent=initialize_agent(
-        llm=get_claude3(),
-        agent="zero-shot-react-description",
-        tools=tools,
-        verbose=True,
-        max_iteration=2,
-        return_intermediate_steps=True,
-        handle_parsing_errors=True,
-        output_key="output",
-    )
-
-
-    prompt="""Human: You are a financial advisor. Give stock recommendations for given query based on following instructions. 
+template="""Human: You are a financial advisor. Give stock recommendations for given query based on following instructions. 
 <instructions>
 Answer the following questions as best you can. You have access to the following tools:
 
-get company ticker: Use when you need to extract company name and stock ticker. This tool will output company name and stock ticker. You should input the human input to it.
-get stock data: Use when you are asked to evaluate or analyze a stock. This will output historic share price data. You should input the stock ticker to it.
-get recent news: Use this to fetch recent news about stocks. This will output company stock news. You should innput the company name to it.
-get financial statements: Use this to get financial statement of the company. With the help of this data companys historic performance can be evaluaated. You should input stock ticker to it
+{tools}
+
 </instructions>
 
 <steps>
 Note- if you fail in satisfying any of the step below, Just move to next one
 1) Use "get company ticker" tool to get the company name and stock ticker. Output- company name and stock ticker
-2) Use "get stock data" tool to gather stock info. Output- Stock data
-3) Use "get recent news" tool to search for latest stock realted news. Output- Stock news
-4) Use "get financial statements" tool to get company's historic financial data. Output- Financial statement
+2) Use "get stock data" tool to gather financial info. Output- Stock data
+3) Use "get recent news" tool to search for latest stock realted recent news. Output- Stock news
+4) Use "get financial statements" tool to get company's historic financial performance data. Output- Financial statement
 5) Analyze the stock based on gathered data and give detail analysis for investment choice. provide numbers and reasons to justify your answer. Output- Detailed stock Analysis
 </steps>
 
 Use the following format:
 Question: the input question you must answer
 Thought: you should always think about what to do, Also try to follow steps mentioned above
-Action: the action to take, should be one of [get company ticker, get stock data, get recent news, get financial statements]
+Action: the action to take, should be one of [{tool_names}]
 Action Input: the input to the action
 Observation: the result of the action
-... (this Thought/Action/Action Input/Observation can repeat N times)
+... (this Thought/Action/Action Input/Observation can not repeat)
 Thought: I now know the final answer
-Final Answer: the final answer to the original input question
+Final Answer: the final answer and with detail explanation to the original input question. 
 
 Question: {input}
 
@@ -273,6 +259,20 @@ Assistant:
 {agent_scratchpad}
 
 """
-    agent.agent.llm_chain.prompt.template=prompt
-    return agent
+
+from langchain.agents import AgentExecutor, create_react_agent 
+
+def initializeAgent():
+    prompt = PromptTemplate.from_template(template)
+    agent = create_react_agent(llm=get_claude3(), tools=tools, prompt=prompt)
+    agent_executor = AgentExecutor(
+        agent=agent,
+        max_iterations=5,
+        tools=tools, 
+        verbose=True, 
+        handle_parsing_errors=True
+    )
+
+    return agent_executor
+
     
