8f2c28ebf5615f826d826941fd564a1f8325d9ae
chimera01 <60414400+chimera01@users.noreply.github.com>
2024-05-10 17:08:39 +0900

    Update stock_analysis_app.py

--- 4_stock_analysis/stock_analysis_app.py
+++ 4_stock_analysis/stock_analysis_app.py
@@ -7,9 +7,9 @@ import pandas as pd
 
 def print_result(st, response):
     try:
-        st.subheader("Daily sticker:")
+        st.subheader("주가 데이터:")
         st.dataframe(response['intermediate_steps'][1][1])
-        st.subheader("Stock Chart:")
+        st.subheader("주식 차트:")
         df = pd.DataFrame(response['intermediate_steps'][1][1],columns=['Close','Volume'])
         df['Volume'] = df['Volume']/10000000
         df.rename(columns={'Close':'Price(USD)','Volume':'Volume(10 millions)'},inplace=True)
@@ -21,8 +21,8 @@ def print_result(st, response):
 
 
 def stock_analysis():
-    st.header("Stock Analysis Agent")
-    st.write("Try to input with company name like Amazon, Tesla, Apple..etc")
+    st.header("주식 분석 에이전트")
+    st.write("회사 이름을 입력해 주세요. Amazon, Tesla, Apple, 삼성전자, NAVER, 카카오, KB금융 등")
 
     if 'database' not in st.session_state: 
         with st.spinner("Initial Database"): 
@@ -32,7 +32,7 @@ def stock_analysis():
         st.session_state.chat_history = [] 
 
     agent = glib.initializeAgent()
-    input_text = st.chat_input("Type company name here!") 
+    input_text = st.chat_input("검색하고 싶은 상장 주식(국내주식, 해외주식)의 회사 이름을 입력해 주세요!") 
     ph = st.empty()
     if input_text:
         ph.empty()
